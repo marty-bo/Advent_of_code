a829be8fb1353823d12e1d64c70e34f5b6c21812
marty <boyer.marty38@gmail.com>
2020-12-20 22:23:52 +0100

    day 17 part 2 not optimised

--- 2020/17/17.py
+++ 2020/17/17.py
@@ -2,7 +2,13 @@
 import time
 
 
-# Part 1 - Game of life for an 3d environment
+
+
+# ################################################################
+#
+# Part 1 - Game of life in a 3d environment
+#
+# ################################################################
 
 # add layer of 0 on faces of the cube C if there is a 1 in the face
 # C[x][y][z]
@@ -139,8 +145,11 @@ def f1(fileName, cycleLimit):
 
 
 
-
-# PART 2 - Generic Function for any dimension (at least >= 2).
+# ################################################################
+#
+# PART 2 - Game of life in a n-dimension environment (at least 2d).
+#
+# ################################################################
 
 # transform a slice (=matrix)(the input) into a <d> dimension array
 def sliceToNDimensionCube(s : list, d : int):
@@ -153,34 +162,126 @@ def sliceToNDimensionCube(s : list, d : int):
     return s
 
 
-# generate an <d> dimension array of size <size>
-def generateNDimensionArray(d : int, size : list):
-    if d == 0:
+# generate an <d> dimension array of size <size> filled with 0
+def generateNDimensionArray(size : list):
+    if size == []:
         return 0
     else:
-        r = []
-        for i in range(size[0]):
-            r.append(generateNDimensionArray(d-1, size[1:]))
-        return  r
+        return [generateNDimensionArray(size[1:]) for i in range(size[0])]
 
 
-
-# return a tuple (dimension, [sizes]).
-def getArrayDimensionAndSize(A : list) -> tuple:
+# return a list [sizes].
+def getArraySize(A : list) -> list:
     if not isinstance(A, list):
-        return (0, [])
+        return []
     else:
-        r = getArrayDimensionAndSize(A[0])
-        r[1].insert(0, len(A))
-        return (r[0]+1, r[1])
+        r = getArraySize(A[0])
+        r.insert(0, len(A))
+        return r
 
+# print(getArraySize([[[1,2,3]]]))
 
 
 # for each list L, insert at the start and at the end a list of 0 with the same dimension as other list in L
-def expandNDimensionArray(d : int, A : list):
-    pass
+# TODO : add the list only if there is a 1 in the face
+def expandNDimensionArray(A : list):
+    if not isinstance(A, list):
+        return
+    for l in A:
+        expandNDimensionArray(l)
+    v = getArraySize(A[0])
+    A.insert(0, generateNDimensionArray(v))
+    A.append(generateNDimensionArray(v))
+
+# a = [[1,1]]
+# expandNDimensionArray(a)
+# print(a)
 
 
+
+
+# copy a n-dimension array
+def copyNDimensionArray(A : list):
+    if not isinstance(A, list):
+        return A
+    else:
+        return [copyNDimensionArray(a) for a in A]
+
+
+
+def getNeighborsCount(A : list, pos : list) -> int:
+    if not isinstance(A, list):
+        return int(A != 0)
+    else:
+        n = 0
+        if pos[0] > 0:
+            n += getNeighborsCount(A[pos[0]-1], pos[1:])
+        if pos[0] < len(A)-1:
+            n += getNeighborsCount(A[pos[0]+1], pos[1:])
+        n += getNeighborsCount(A[pos[0]], pos[1:])
+        return n
+
+# return the value of a case at pos <pos>
+def getValueAtPos(A : list, pos : list):
+    if not isinstance(A, list):
+        return int(A != 0)
+    else:
+        return getValueAtPos(A[pos[0]], pos[1:])
+
+
+# set value at pos <pos> in a n-dimension array <A>
+def setValueAtPos(A : list, pos : list, val : int):
+    if not isinstance(A[pos[0]], list):
+        A[pos[0]] = val
+    else:
+        setValueAtPos(A[pos[0]], pos[1:], val)
+
+
+
+def cycleCore(A : list, cycleLimit : int):
+
+    # for each cycle
+    while cycleLimit:
+        expandNDimensionArray(A)
+        A_copy = copyNDimensionArray(A)
+        
+        # get the size of the n-dimension array A
+        size = getArraySize(A_copy)
+        n = len(size)
+        # generate a list of n values representing the pos in the n-dimension array
+        pos = [0] * n
+
+        # for each case
+        while pos[0] != size[0]:
+            nb_neighbors = getNeighborsCount(A, pos)
+            alive = getValueAtPos(A, pos)
+            nb_neighbors -= alive
+            if alive:
+                if nb_neighbors < 2 or nb_neighbors > 3:
+                    setValueAtPos(A_copy, pos, 0)
+            else:
+                if nb_neighbors == 3:
+                    setValueAtPos(A_copy, pos, 1)
+
+            # increment the position
+            pos[-1] += 1
+            for i in range(n-1, 0, -1):
+                if pos[i] == size[i]:
+                    if i != 0:
+                        pos[i-1] += 1
+                        pos[i] = 0
+        # next cycle
+        cycleLimit -= 1
+        # copy the new array
+        A = A_copy
+    return A
+    
+# count number of values != 0 in an n-dimension array
+def countAliveCube(A : list):
+    if not isinstance(A, list):
+        return A != 0
+    else:
+        return sum([countAliveCube(a) for a in A])
 
 
 def f2(fileName, cycleLimit, dimension):
@@ -191,13 +292,14 @@ def f2(fileName, cycleLimit, dimension):
         s.append([1 if c == '#' else 0 for c in l.split()[0]])
         l = f.readline()
     C = sliceToNDimensionCube(s, dimension)
-    print(C)
+    C = cycleCore(C, cycleLimit)
+    return countAliveCube(C)
 
 
 
 
 # ####### MAIN #######
-f = 'bis_input.txt'
+f = 'input.txt'
 
 # Part 1
 # Game of life in 3d space
@@ -205,9 +307,10 @@ start_time = time.time()
 print(f1(f, 6))
 print("--- %s seconds ---" % (time.time() - start_time))
 
-# Part 2
-# Game of life in 4d space
+# Part 2 - not well optimised
+# Game of life in n-dimension space
 start_time = time.time()
+# 6 cycle in a 4d space
 print(f2(f, 6, 4))
 print("--- %s seconds ---" % (time.time() - start_time))
 
