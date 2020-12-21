fd5c5a15d6bd9daf96cfdd3723787ad73d52a3bc
marty <boyer.marty38@gmail.com>
2020-12-21 14:51:42 +0100

    day 18 part 2

--- 2020/18/18.py
+++ 2020/18/18.py
@@ -3,47 +3,6 @@ import time
 
 
 
-# ################################################################
-#
-# Part 1 - LL1 analyze
-#
-# ##### Grammar :
-# R0 : S → E [EOL/EOF]
-# R1 : E → E + F  
-# R2 : E → E * F  
-# R3 : E → F
-# R4 : F → val
-# R5 : F → ( E )
-#
-# ##### Without left recursivity :
-# R0 : S → E [EOL/EOF]
-# R1 : E → F E2
-# R2 : E2 → + F E2 
-# R3 : E2 → * F E2
-# R4 : E2 → epsilon
-# R5 : F → val
-# R6 : F → ( E )
-#
-# ##### first :
-#     +   *   (   )   val   epsilon
-# E           x       x
-# E2  x   x                 x
-# F           x       x
-#
-# ##### next :
-#     +   *   (   )   val     [EOL/EOF]
-# E               x           x         
-# E2              x           x      
-# F   x   x       x           x
-#
-# ##### LL1 :
-#     +   *   (   )   val     [EOL/EOF]
-# S           R0      R0
-# E           R1      R1
-# E2  R2  R3      R4          R4
-# F           R6      R5
-# 
-# ################################################################
 
 
 class Reader:
@@ -64,30 +23,76 @@ def isVal(str_):
     except:
         return 0
 
-# R0 : S → E [EOL/EOF]
-def S(reader):
+
+# ################################################################
+#
+# Part 1 - LL1 analyze
+#
+# ##### Grammar WITHOUT operator priority :
+# S → E END
+# E → E + F  
+# E → E * F  
+# E → F
+# F → val
+# F → ( E )
+#
+# ##### Without left recursivity :
+# R0 : S → E END
+# R1 : E → F E2
+# R2 : E2 → + F E2 
+# R3 : E2 → * F E2
+# R4 : E2 → epsilon
+# R5 : F → val
+# R6 : F → ( E )
+#
+# ##### first :
+#     +   *   (   )   val   epsilon
+# E           x       x
+# E2  x   x                 x
+# F           x       x
+#
+# ##### next :
+#     +   *   (   )   val     END
+# E               x           x         
+# E2              x           x      
+# F   x   x       x           x
+#
+# ##### LL1 :
+#     +   *   (   )   val     END
+# S           R0      R0
+# E           R1      R1
+# E2  R2  R3      R4          R4
+# F           R6      R5
+# 
+# ################################################################
+
+
+
+
+# R0 : S → E END
+def S_p1(reader):
     if reader.look() == '(' or isVal(reader.look()):
         # E
-        r = E(reader)
-        # [EOL/EOF]
+        r = E_p1(reader)
+        # END
         if reader.isEmpty():
             return r
     return -1
 
 # R1 : E → F E2
-def E(reader):
+def E_p1(reader):
     if reader.look() == '(' or isVal(reader.look()):
         # F
-        v = F(reader)
+        v = F_p1(reader)
         # E2
-        r = E2(reader, v)
+        r = E2_p1(reader, v)
         return r
     return -1
 
 # R2 : E2 → + F E2 
 # R3 : E2 → * F E2
 # R4 : E2 → epsilon
-def E2(reader, val):
+def E2_p1(reader, val):
     if reader.isEmpty() or reader.look() == ')':
         # epsilon
         return val
@@ -95,28 +100,28 @@ def E2(reader, val):
         # +
         reader.read()
         # F
-        val += F(reader)
+        val += F_p1(reader)
         # E2
-        r = E2(reader, val)
+        r = E2_p1(reader, val)
         return r
     elif reader.look() == '*':
         # *
         reader.read()
         # F
-        val *= F(reader)
+        val *= F_p1(reader)
         # E2
-        r = E2(reader, val)
+        r = E2_p1(reader, val)
         return r
     return -1
 
 # R5 : F → val
 # R6 : F → ( E )
-def F(reader):
+def F_p1(reader):
     if reader.look() == '(':
         # (
         reader.read()
         # E
-        r = E(reader)
+        r = E_p1(reader)
         # )
         reader.read()
         return r
@@ -132,9 +137,159 @@ def f1(fileName):
     # print(expressions)
     res = []
     for expr in expressions:
-        res.append(S(Reader(expr)))
+        res.append(S_p1(Reader(expr)))
     return sum(res)
 
+
+
+
+
+
+
+# ################################################################
+#
+# Part 2 - LL1 analyze
+#
+# ##### Grammar WITH operator priority :
+# Attention '+' is more priority than '*'
+# S → E END
+# E → E * T 
+# E → T  
+# T → T + F  
+# T → F
+# F → val
+# F → ( E )
+#
+# ##### Without left recursivity :
+# R0 : S → E END
+# R1 : E → T E2
+# R2 : E2 → + T E2
+# R3 : E2 → epsilon
+# R4 : T → F T2
+# R5 : T2 → * F T2
+# R6 : T2 → epsilon
+# R7 : F → val
+# R8 : F → ( E )
+#
+# ##### first :
+#     +   *   (   )   val     epsilon
+# E           x       x
+# E2      x                   x
+# T           x       x
+# T2  x                       x
+# F           x       x
+#
+# ##### next :
+#     +   *   (   )   val     END
+# E               x           x         
+# E2              x           x    
+# T       x       x           x
+# T2      x       x           x
+# F   x   x       x           x
+#
+# ##### LL1 :
+#     +   *   (   )   val     END
+# S           R0      R0
+# E           R1      R1
+# E2      R2      R3          R3
+# T           R4      R4
+# T2  R5  R6      R6          R6
+# F           R6      R5
+# 
+# ################################################################
+
+# R0 : S → E END
+def S_p2(reader):
+    if reader.look() == '(' or isVal(reader.look()):
+        # E
+        r = E_p2(reader)
+        # END
+        if reader.isEmpty():
+            return r
+    return -1
+
+# R1 : E → T E2
+def E_p2(reader):
+    if reader.look() == '(' or isVal(reader.look()):
+        # T
+        v = T_p2(reader)
+        # E2
+        r = E2_p2(reader, v)
+        return r
+    return -1
+
+
+# R2 : E2 → * T E2
+# R3 : E2 → epsilon
+def E2_p2(reader, val):
+    if reader.isEmpty() or reader.look() == ')':
+        # epsilon
+        return val
+    elif reader.look() == '*':
+        # *
+        reader.read()
+        # T
+        val *= T_p2(reader)
+        # E2
+        r = E2_p2(reader, val)
+        return r
+    return -1
+
+
+# R4 : T → F T2
+def T_p2(reader):
+    if reader.look() == '(' or isVal(reader.look()):
+        # F
+        v = F_p2(reader)
+        # T2
+        r = T2_p2(reader, v)
+        return r
+    return -1
+
+
+
+# R5 : T2 → + F T2
+# R6 : T2 → epsilon
+def T2_p2(reader, val):
+    if reader.isEmpty() or reader.look() == ')' or reader.look() == '*':
+        # epsilon
+        return val
+    elif reader.look() == '+':
+        # +
+        reader.read()
+        # F
+        val += F_p2(reader)
+        # T2
+        r = T2_p2(reader, val)
+        return r
+    return -1
+
+
+# R5 : F → val
+# R6 : F → ( E )
+def F_p2(reader):
+    if reader.look() == '(':
+        # (
+        reader.read()
+        # E
+        r = E_p2(reader)
+        # )
+        reader.read()
+        return r
+    elif isVal(reader.look()):
+        # val
+        r = int(reader.read())
+        return r
+    return -1
+
+def f2(fileName):
+    f = open(fileName, 'r')
+    expressions = [ list(l.replace(' ', '').replace('\n', '')) for l in f.readlines()]
+    # print(expressions)
+    res = []
+    for expr in expressions:
+        res.append(S_p2(Reader(expr)))
+    return sum(res)
 
 
 # ####### MAIN #######
@@ -143,4 +298,9 @@ f = 'input.txt'
 # Part 1
 start_time = time.time()
 print(f1(f))
+print("--- %s seconds ---" % (time.time() - start_time))
+
+# Part 2
+start_time = time.time()
+print(f2(f))
 print("--- %s seconds ---" % (time.time() - start_time))
