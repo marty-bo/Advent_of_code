21d102b75fff8745a960cf68503a5330d82a7452
marty <boyer.marty38@gmail.com>
2020-12-27 11:44:54 +0100

    day 22 part 2 not optimised

--- 2020/22/22.py
+++ 2020/22/22.py
@@ -1,8 +1,7 @@
 import time
 
 
-
-def f1(fileName):
+def readCards(fileName):
     p1_cards = []
     p2_cards = []
 
@@ -20,8 +19,17 @@ def f1(fileName):
         p2_cards.append(int(l[0]))
         l = f.readline().split()
     
-    # print(p1_cards)
-    # print(p2_cards)
+    return p1_cards, p2_cards
+
+def score(deck):
+    res = 0
+    for i in range(len(deck)):
+        res += deck[len(deck)-1-i] * (i+1)
+    return res
+
+
+def f1(fileName):
+    p1_cards, p2_cards = readCards(fileName)
 
     while p1_cards and p2_cards:
         c1 = p1_cards.pop(0)
@@ -36,20 +44,101 @@ def f1(fileName):
     
     # print(p1_cards)
     # print(p2_cards)
+   
+    return score(p1_cards + p2_cards)
 
-    w = p1_cards + p2_cards
-    # print(w)
 
-    res = 0
-    for i in range(len(w)):
-        res += w[len(w)-1-i] * (i+1)
-    return res
+
+def is_superior(c1, c2):
+    return c1 > c2
+
+def is_inferior(c1, c2):
+    return c1 < c2
+
+def round_in_history(history, current):
+    for old in history:
+        if len(current[0]) == len(old[0]):
+            b = True
+            for i in range(len(current[0])):
+                if current[0][i] != old[0][i]:
+                    b = False
+                    break
+            if not b:
+                continue
+            for i in range(len(current[1])):
+                if current[1][i] != old[1][i]:
+                    break
+            if not b:
+                continue
+            # print(old)
+            return 1
+    return 0
+
+def recursive_combat(p1_cards, p2_cards, compare, game=0):
+    history = []
+    # print('Enter a combat.')
+    # r = 0
+    while p1_cards and p2_cards:
+        # print('Game :', game, 'round :', r)
+        # r += 1
+        if round_in_history(history, [p1_cards, p2_cards]):
+            # print(history)
+            # print(p1_cards)
+            # print(p2_cards)
+            # print('in history')
+            return 0
+        history.append([p1_cards.copy(), p2_cards.copy()])
+
+        c1 = p1_cards.pop(0)
+        c2 = p2_cards.pop(0)
+
+        # print(c1, p1_cards)
+        # print(c2, p2_cards)
+        # print('')
+
+        if len(p1_cards) >= c1 and len(p2_cards) >= c2:
+            if recursive_combat(p1_cards[:c1].copy(), p2_cards[:c2].copy(), is_superior, game + 1):
+                p2_cards.append(c2)
+                p2_cards.append(c1)
+            else:   
+                p1_cards.append(c1)
+                p1_cards.append(c2)
+        elif compare(c1, c2):
+            p1_cards.append(c1)
+            p1_cards.append(c2)
+        else:
+            p2_cards.append(c2)
+            p2_cards.append(c1)
+    
+    if p1_cards:
+        return 0
+    else:
+         return 1
+    
+
+
+def f2(fileName):
+    p1_cards, p2_cards = readCards(fileName)
+    if recursive_combat(p1_cards, p2_cards, is_superior):
+        return score(p2_cards)
+    else:
+        return score(p1_cards)
+
+
+
+
 
 # ####### MAIN #######
 f = 'input.txt'
 
-# Part 1-2
+# Part 1
 start_time = time.time()
 res1 = f1(f)
 print(res1)
+print("--- %s seconds ---" % (time.time() - start_time))
+
+# Part 2
+start_time = time.time()
+res2 = f2(f)
+print(res2)
 print("--- %s seconds ---" % (time.time() - start_time))
