e93f9579ed53460e0b11f5d5fb5611aa90c39210
marty <boyer.marty38@gmail.com>
2021-06-11 18:47:33 +0200

    2019 day 04 part 2

--- 2019/04/04.py
+++ 2019/04/04.py
@@ -6,7 +6,7 @@ import time
 FILE1 = "input_1.txt"
 
 # check if val is valid
-def validation(val):
+def validation_part1(val):
     double = False
     prev = -1
     for i in range(len(val)):
@@ -16,6 +16,34 @@ def validation(val):
             return False
         prev = val[i]
     return double
+
+
+# check if val is valid
+def validation_part2(val):
+    double = False
+    tmpDouble = False
+    state = 0
+    prev = -1
+    for i in range(len(val)):
+        if val[i] == prev:
+            if state == 0:
+                tmpDouble = True
+                state = 1
+            elif state == 1:
+                tmpDouble = False
+                state = 2
+        elif val[i] < prev:
+            return False
+        else:
+            if state == 1:
+                double = True
+            state = 0
+        prev = val[i]
+
+    if state == 1:
+        double = True
+    return double
+
 
 # return True is valMin is less than valMax, False otherwise
 def inferior(valMin, valMax):
@@ -50,17 +78,30 @@ def f1(valMin, valMax):
         if not increment(valMin):
             print("Overflow")
             return
-        if validation(valMin):
+        if validation_part1(valMin):
             count += 1
     return count
 
-        
+      
+def f2(valMin, valMax):
+    count = 0
+    while inferior(valMin, valMax):
+        if not increment(valMin):
+            print("Overflow")
+            return
+        if validation_part2(valMin):
+            count += 1
+    return count  
 
 
 
 def main(filename1):
     start_time = time.time()
     print(f1(*[[int(c) for c in list(v)] for v in open(filename1).readline().split("-")]))
+    print("--- %s seconds ---" % (time.time() - start_time))
+
+    start_time = time.time()
+    print(f2(*[[int(c) for c in list(v)] for v in open(filename1).readline().split("-")]))
     print("--- %s seconds ---" % (time.time() - start_time))
 
 main(FILE1)
