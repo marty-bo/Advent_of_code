216a4c127e0c1278599c09952e7ec348ac0c9886
marty <boyer.marty38@gmail.com>
2021-06-13 12:08:02 +0200

    2019 day 06 part 2

--- 2019/06/06.py
+++ 2019/06/06.py
@@ -3,9 +3,11 @@ import time
 
 
 class Node:
-    def __init__(self, val):
+    def __init__(self, val, p):
         self.value = val
         self.satellites = dict()
+        # for part 2 only
+        self.parent = p
 
 
 
@@ -22,19 +24,19 @@ def f1(fileName):
     s = file.readline().replace('\n', '').split(')')
     # build the tree
     objects = dict()
-    p = Node(s[0])
-    c = Node(s[1])
+    p = Node(s[0], None)
+    c = Node(s[1], None)
     p.satellites[s[1]] = c
     objects[s[0]] = p
     objects[s[1]] = c
     while s != ['']:
         p = objects.get(s[0])
         if p == None:
-            p = Node(s[0])
+            p = Node(s[0], None)
             objects[s[0]] = p
         c = objects.get(s[1])
         if c == None:
-            c = Node(s[1])
+            c = Node(s[1], None)
             objects[s[1]] = c
         p.satellites[s[1]] = c
         s = file.readline().replace('\n', '').split(')')
@@ -54,10 +56,58 @@ def f1(fileName):
 
 
 
+def f2(fileName):
+    file = open(fileName)
+    s = file.readline().replace('\n', '').split(')')
+    # build the tree
+    objects = dict()
+    p = Node(s[0], None)
+    c = Node(s[1], p)
+    p.satellites[s[1]] = c
+    objects[s[0]] = p
+    objects[s[1]] = c
+    while s != ['']:
+        p = objects.get(s[0])
+        if p == None:
+            p = Node(s[0], None)
+            objects[s[0]] = p
+        c = objects.get(s[1])
+        if c == None:
+            c = Node(s[1], p)
+            objects[s[1]] = c
+        c.parent = p
+        p.satellites[s[1]] = c
+        s = file.readline().replace('\n', '').split(')')
+
+    # get the path of 'YOU'
+    node = objects.get('YOU')
+    pathYOU = []
+    while node.parent != None:
+        node = node.parent
+        pathYOU.insert(0, node.value)
+    # get the path of 'SAN'
+    node = objects.get('SAN')
+    pathSAN = []
+    while node.parent != None:
+        node = node.parent
+        pathSAN.insert(0, node.value)
+    
+    i = 0
+    while i<len(pathSAN) and i<len(pathYOU) and pathSAN[i]==pathYOU[i]:
+        i += 1
+    return (len(pathSAN)-i) + (len(pathYOU)-i)
+
 
 # Part 1
 f = 'input_1.txt'
 start_time = time.time()
 print(f1(f))
+print("--- %s seconds ---" % (time.time() - start_time))
+
+
+# Part 2
+f = 'input_2.txt'
+start_time = time.time()
+print(f2(f))
 print("--- %s seconds ---" % (time.time() - start_time))
 
