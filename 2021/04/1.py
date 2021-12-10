991fa309db307ce628b6bcb425a456f4a90e2303
marty <boyer.marty38@gmail.com>
2021-12-10 18:23:06 +0100

    2021 day 04 part 2

--- 2021/04/1.py
+++ 2021/04/1.py
@@ -38,9 +38,48 @@ def f1():
 
 
 
+def f2():
+    f = open('input.txt', 'r')
+    
+    numbers = [int(n) for n in f.readline().replace('\n','').split(',')]
+    f.readline()
+
+    # list of all boards on the game
+    boards = []
+
+    tmp = []
+    for line in f:
+        line = [int(n) for n in line.split()]
+        if len(line) == 0:
+            boards.append(board.Board(tmp))
+            tmp = []
+        else:
+            tmp.append(line)
+        
+    f.close()
+
+    winScore = 0
+    for n in numbers:
+        for b in boards[::-1]:
+            b.tick(n)
+            if b.hasWin():
+                boards.remove(b)
+                if len(boards) == 0:
+                    winScore = n * b.getSumOfUnticked()
+        if winScore != 0:
+            break
+
+    print('[f2]: winScore = {}\n'.format(winScore))
+    return
+
 
 print("------------------")
 start_time = time.time()
 f1()
+print("--- %s seconds ---" % (time.time() - start_time))
+
+print("------------------")
+start_time = time.time()
+f2()
 print("--- %s seconds ---" % (time.time() - start_time))
 
